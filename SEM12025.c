2282e01c8065b54ee4a8a21dc812b694d3d5b64a
Pranshu Raj Goel <pranshurajgoel@gmail.com>
2025-05-07 14:24:08 +0930

    Update SEM12025.c

--- SEM12025.c
+++ SEM12025.c
@@ -1,67 +1,63 @@
 #include<stdio.h>
 
-// int cWorkshopWeek1(void) {
-//     // single line Comment
-//     /*
-//      *   Multi line comment
-//      *   Multi line comment
-//      *   Multi line comment
-//      *   Multi line comment
-//      *   Multi line comment
-//      */
+int cWorkshopWeek1(void) {
+    // single line Comment
+    /*
+     *   Multi line comment
+     *   Multi line comment
+     *   Multi line comment
+     *   Multi line comment
+     *   Multi line comment
+     */
 
-//     int a = 123;
-//     float b = 3.141;
-//     double c = 1.14125698;
-//     long d = 841498659849684;
-//     char e = 'h';
-//     char f[10] = "C Prgming";
+    int a = 123;
+    float b = 3.141;
+    double c = 1.14125698;
+    long d = 841498659849684;
+    char e = 'h';
+    char f[10] = "C Prgming";
  
-//     int g;
-//     float h;
-//     double i;
-//     long j;
-//     char k;
-//     char l[10];
+    int g;
+    float h;
+    double i;
+    long j;
+    char k;
+    char l[10];
     
-//     printf("Enter a int: ");
-//     scanf("%d" , &g);
-//     printf("a: %d \t g: %d\n", a, g);
+    printf("Enter a int: ");
+    scanf("%d" , &g);
+    printf("a: %d \t g: %d\n", a, g);
     
-//     printf("Enter a float: ");
-//     scanf("%f" , &h);
-//     printf("b: %f \t h: %f\n", b, h);
+    printf("Enter a float: ");
+    scanf("%f" , &h);
+    printf("b: %f \t h: %f\n", b, h);
     
-//     printf("Enter a double: ");
-//     scanf("%lf", &i);
-//     printf("c: %lf \t i: %lf\n", c, i);
+    printf("Enter a double: ");
+    scanf("%lf", &i);
+    printf("c: %lf \t i: %lf\n", c, i);
     
-//     printf("Enter a long: ");
-//     scanf("%ld" , &j);
-//     printf("d: %ld \t j: %ld\n", d, j);
+    printf("Enter a long: ");
+    scanf("%ld" , &j);
+    printf("d: %ld \t j: %ld\n", d, j);
     
-//     printf("Enter a char: ");
-//     scanf(" %c" , &k);
-//     printf("e: %c \t k: %c\n", e, k);
-//     printf("e: %d \t k: %d\n", e, k);
+    printf("Enter a char: ");
+    scanf(" %c" , &k);
+    printf("e: %c \t k: %c\n", e, k);
+    printf("e: %d \t k: %d\n", e, k);
     
-//     printf("Enter a string: ");
-//     scanf("%s" ,  l);
-//     printf("f: %s \t l: %s\n", f, l);
+    printf("Enter a string: ");
+    scanf("%s" ,  l);
+    printf("f: %s \t l: %s\n", f, l);
     
     
     
-//     int m;
-//     float n;
-//     char o[120];
-//     printf("Enter the input vales for m, n, o space seperated: ");
-//     scanf("%d %f %s", &m, &n, o);
-//     printf("m: %d \t n: %f \t o: %s \n", m, n, o);
+    int m;
+    float n;
+    char o[120];
+    printf("Enter the input vales for m, n, o space seperated: ");
+    scanf("%d %f %s", &m, &n, o);
+    printf("m: %d \t n: %f \t o: %s \n", m, n, o);
     
-//     return 0;
-// }
-
-int main(void) {
     float kCalueOfPi = 3.141;
     float radius_of_circle;
     printf("Enter the radius of circle: ");
