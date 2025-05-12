aa94e21ad0df22ca617a80b76e7377851d4c6588
Pranshu Raj Goel <pranshurajgoel@gmail.com>
2025-05-12 14:21:10 +0930

    Create if_else.c

---
+++ week9/if_else.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Syntax:
+    // if(expression) {
+    //     if_true_statements;
+    // }
+    // else {
+    //     else_false_statements;
+    // }
+    
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("The number %d is an ");
+    if(num % 2 == 0) {
+        printf("even number\n");
+    }
+    else {
+        printf("odd number\n")
+    }
+}
